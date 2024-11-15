db5d89963aad4fde274529983ff685fcbf142e73
Paul Kloetzing <kottmitkott@gmail.com>
2024-11-15 21:57:28 +0100

    ADDED jump and whale

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,12 @@ namespace SuperFrank
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private float _movementFactor = 5.0f;
+        [SerializeField] private float _movementSpeed = 5.0f;
+        [SerializeField] private float _movementDamping = 10.0f;
         [SerializeField] private float _aimSensitivity = 1.0f;
+
+        [SerializeField] private float _gravityForce = 10.0f;
+        [SerializeField] private float _jumpVelocity = 30.0f;
 
         [SerializeField] private float _baseHeight = 10.0f;
         [SerializeField] private Transform _cameraPivot;
@@ -13,16 +17,28 @@ namespace SuperFrank
         [SerializeField] private float _camPitchStrength = 1.0f;
         [SerializeField] private float _camPitchSmoothTime = 1.0f;
 
-        private Quaternion _statePosition = Quaternion.identity;
-        private float _height;
+        [SerializeField] private Animator _animator;
+
+        private Quaternion _positionState = Quaternion.identity;
+        private float _heightState;
+
+        private Vector3 _positionEulerVelocity;
+        private float _heightVelocity;
 
         private float _camPitchCurrent;
         private float _camPitchVelocity;
+
+        private static readonly int _speedAnimId = Animator.StringToHash("speed");
 
         private void Awake()
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+        }
+
+        private void Start()
+        {
+            _heightState = _baseHeight;
         }
 
         private void Update()
@@ -32,20 +48,42 @@ namespace SuperFrank
 
             Vector2 mouseVector = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
+            bool jumpPressed = Input.GetButtonDown("Jump");
+
             float camPitchTarget = -mouseVector.y * _camPitchStrength;
             _camPitchCurrent = Mathf.SmoothDamp(_camPitchCurrent, camPitchTarget, ref _camPitchVelocity, _camPitchSmoothTime, 100.0f);
             _cameraPivot.localRotation = Quaternion.Euler(_camPitchCurrent, 0.0f, 0.0f);
 
-            _statePosition *= Quaternion.Euler(
-                inputVector.y * _movementFactor * Time.deltaTime,
-                mouseVector.x * _aimSensitivity * Time.deltaTime,
-                -inputVector.x * _movementFactor * Time.deltaTime
+            _positionEulerVelocity.x += inputVector.y * _movementSpeed * _movementDamping * Time.deltaTime;
+            _positionEulerVelocity.z += -inputVector.x * _movementSpeed * _movementDamping * Time.deltaTime;
+
+            _positionEulerVelocity.y = mouseVector.x * _aimSensitivity;
+
+            _heightVelocity -= _gravityForce * Time.deltaTime;
+            if (jumpPressed)
+            {
+                _heightVelocity = _jumpVelocity;
+            }
+
+            _positionState *= Quaternion.Euler(
+                _positionEulerVelocity.x * Time.deltaTime,
+                _positionEulerVelocity.y * Time.deltaTime,
+                _positionEulerVelocity.z * Time.deltaTime
                 );
 
-            _height = _baseHeight;
+            _positionEulerVelocity.x /= 1.0f + _movementDamping * Time.deltaTime;
+            _positionEulerVelocity.z /= 1.0f + _movementDamping * Time.deltaTime;
 
-            transform.position = _statePosition * Vector3.up * _height;
-            transform.rotation = _statePosition;
+            _heightState += _heightVelocity * Time.deltaTime;
+
+            if (_heightState < _baseHeight)
+            {
+                _heightState = _baseHeight;
+                _heightVelocity = 0.0f;
+            }
+
+            transform.position = _positionState * Vector3.up * _heightState;
+            transform.rotation = _positionState;
         }
     }
 }
