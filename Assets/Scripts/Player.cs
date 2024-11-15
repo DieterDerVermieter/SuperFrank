2108279e8489fa751ae75744ea0020d68f8f7a0e
Paul Kloetzing <kottmitkott@gmail.com>
2024-11-15 21:16:47 +0100

    ADDED player third person aim

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,37 @@ namespace SuperFrank
         [SerializeField] private float _aimSensitivity = 1.0f;
 
         [SerializeField] private float _baseHeight = 10.0f;
+        [SerializeField] private Transform _cameraPivot;
+
+        [SerializeField] private float _camPitchStrength = 1.0f;
+        [SerializeField] private float _camPitchSmoothTime = 1.0f;
 
         private Quaternion _statePosition = Quaternion.identity;
         private float _height;
 
+        private float _camPitchCurrent;
+        private float _camPitchVelocity;
+
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
         private void Update()
         {
-            Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Vector2 inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             inputVector = inputVector.normalized;
+
+            Vector2 mouseVector = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+            float camPitchTarget = -mouseVector.y * _camPitchStrength;
+            _camPitchCurrent = Mathf.SmoothDamp(_camPitchCurrent, camPitchTarget, ref _camPitchVelocity, _camPitchSmoothTime, 100.0f);
+            _cameraPivot.localRotation = Quaternion.Euler(_camPitchCurrent, 0.0f, 0.0f);
 
             _statePosition *= Quaternion.Euler(
                 inputVector.y * _movementFactor * Time.deltaTime,
-                0.0f,
+                mouseVector.x * _aimSensitivity * Time.deltaTime,
                 -inputVector.x * _movementFactor * Time.deltaTime
                 );
 
